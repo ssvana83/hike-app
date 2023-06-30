4a84f75ae3444bcab77fa387d34f3956f76ea59b
Stacy Vana <98608384+ssvana83@users.noreply.github.com>
2023-06-30 14:05:14 -0400

    modifications of CRUD

--- app/controllers/hikes_controller.rb
+++ app/controllers/hikes_controller.rb
@@ -1,47 +1,116 @@
 class HikesController < ApplicationController
   set :default_content_type, 'application/json'
 
+  # get "/hikes" do
+  #     Hike.all.to_json
+  # end
+
+  # added from e to replace above
   get "/hikes" do
-      Hike.all.to_json
+    # render all of the books as JSON
+    Hike.all.to_json(include: [state: {only: [:id, :name]}], except: [:created_at, :updated_at])
   end
 
-  post "/hikes" do
-    hike = Hike.create(params)
-    binding.pry
-    if hike.id
-      hike.to_json(include: :states)
+  # added from e as a new operation completley
+  get "/states/:state_id/hikes" do
+    find_state
+    @state.hikes.to_json(include: [:state])
+  end
+
+  # get "/hikes/:id" do
+  #   begin
+  #     Hike.find(params["id"]).to_json(include: :states)
+  #   rescue ActiveRecord::RecordNotFound => e
+  #     {errors: e}.to_json
+  #    end
+  # end
+
+  # added from e to replace above
+  get "/hikes/:id" do
+    find_hike
+    hike_to_json
+  end
+
+
+  # post "/hikes" do
+  #   hike = Hike.create(params)
+  #   binding.pry
+  #   if hike.id
+  #     hike.to_json(include: :states)
+  #   else
+  #     hike.errors.full_messages.to_sentence
+  #   end
+  # end
+
+  # added from e to replace above
+  post "/states/:state_id/hikes" do
+    find_state
+    @hike = @state.hikes.build(params)
+    if @hike.save
+      hike_to_json
     else
-      hike.errors.full_messages.to_sentence
+      hike_error_messages
     end
   end
 
   
-  get "/hikes/:id" do
-    begin
-      Hike.find(params["id"]).to_json(include: :states)
-    rescue ActiveRecord::RecordNotFound => e
-      {errors: e}.to_json
-     end
-  end
+  # patch "/hikes/:id" do
+  #   hike = Hike.find_by_id(params["id"])
+  #   if hike && hike.update(params)
+  #     hike.to_json(include: :states)
+  #   elsif !hike
+  #     {errors: "Record not found with id provided"}.to_json
+  #   else
+  #     hike.errors.full_messages.to_sentence
+  #   end
+  # end
 
+  # added from e to replace above
   patch "/hikes/:id" do
-    hike = Hike.find_by_id(params["id"])
-    if hike && hike.update(params)
-      hike.to_json(include: :states)
-    elsif !hike
-      {errors: "Record not found with id provided"}.to_json
+    find_hike
+    if @hike.update(params)
+      hike_to_json
     else
-      hike.errors.full_messages.to_sentence
+      hike_error_messages
     end
   end
 
+
+  # delete "/hikes/:id" do
+  #   hike = Hike.find_by_id(params["id"])
+  #   if hike&.destroy
+  #     {errors: "Record successfully destroyed"}.to_json
+  #   else
+  #     {errors: "Record not found with id"}.to_json
+  #   end
+  # end
+# added from e to replace above
   delete "/hikes/:id" do
-    hike = Hike.find_by_id(params["id"])
-    if hike&.destroy
-      {errors: "Record successfully destroyed"}.to_json
+    find_hike
+    if @hike
+      @hike.destroy
+      @hike.to_json
     else
-      {errors: "Record not found with id"}.to_json
+      { errors: ["Hike Doesn't Exist"] }.to_json
     end
   end
+
+  # added additionaly from E
+  private
+    def find_hike
+      @hike = Hike.find_by_id(params["id"])
+    end
+
+    def find_state
+      @state = State.find_by_id(params["state_id"])
+    end
+
+    def hike_to_json
+      @hike.to_json(include: [:state])
+    end
+
+    def hike_error_messages
+      { errors: @hike.errors.full_messages }.to_json
+    end
 
 end
--- app/controllers/states_controller.rb
+++ app/controllers/states_controller.rb
@@ -8,41 +8,95 @@ class StatesController < ApplicationController
       except: [:created_at, :updated_at])
   end
 
+  # get "/states/:id" do
+  #   begin
+  #     State.find(params["id"]).to_json(include: :hikes)
+  #   rescue ActiveRecord::RecordNotFound => e
+  #     {errors: e}.to_json
+  #    end
+  # end
+
+  # ****added from E to replace above
   get "/states/:id" do
-    begin
-      State.find(params["id"]).to_json(include: :hikes)
-    rescue ActiveRecord::RecordNotFound => e
-      {errors: e}.to_json
-     end
+    find_state
+    state_to_json
   end
 
+  # post "/states" do
+  #   state = State.create(params)
+  #   if state.id
+  #     state.to_json(include: :hikes)
+  #   else
+  #     state.errors.full_messages.to_sentence
+  #   end
+  # end
+
+  # ****added from E to replace above
   post "/states" do
-    state = State.create(params)
-    if state.id
-      state.to_json(include: :hikes)
+    @state = State.new(params)
+    if @state.save
+      state_to_json
     else
-      state.errors.full_messages.to_sentence
+      state_error_messages
     end
   end
 
+
+  # patch "/states/:id" do
+  #   state = State.find_by_id(params["id"])
+  #   if state && state.update(params)
+  #     state.to_json(include: :hikes)
+  #   elsif !state
+  #     {errors: "Record not founf with id provided"}.to_json
+  #   else
+  #     state.errors.full_messages.to_sentence
+  #   end
+  # end
+
+  #  *** addedf rom E to replace above
   patch "/states/:id" do
-    state = State.find_by_id(params["id"])
-    if state && state.update(params)
-      state.to_json(include: :hikes)
-    elsif !state
-      {errors: "Record not founf with id provided"}.to_json
+    find_state
+    if @state.update(params)
+      state_to_json
     else
-      state.errors.full_messages.to_sentence
+      state_error_messages
     end
   end
 
+  # delete "/states/:id" do
+  #   state = State.find_by_id(params["id"])
+  #   if state&.destroy
+  #     {errors: "Record successfully destroyed"}.to_json
+  #   else
+  #     {errors: "Record not found with id"}.to_json
+  #   end
+  # end
+
+
+  # added from e to replace above
   delete "/states/:id" do
-    state = State.find_by_id(params["id"])
-    if state&.destroy
-      {errors: "Record successfully destroyed"}.to_json
+    find_state
+    if @state
+      @state.destroy
+      @state.to_json
     else
-      {errors: "Record not found with id"}.to_json
+      { errors: ["State Doesn't Exist"] }.to_json
     end
   end
+
+
+# added from e as addition 
+  private
+    def find_state
+      @state = State.find_by_id(params["id"])
+    end
+
+    def state_to_json
+      @state.to_json(include: [:hikes])
+    end
+
+    def state_error_messages
+      { errors: @state.errors.full_messages }.to_json
+    end
 
 end
